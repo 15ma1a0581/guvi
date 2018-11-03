f6aa3055041cbc3b6c13bb7c4c9c7598294b92bd
15ma1a0581 <43681799+15ma1a0581@users.noreply.github.com>
2018-11-03 11:56:21 +0530

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,17 @@
+class Test
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		if(n>=0)
+		{
+			System.out.println("Positive");
+		}
+		else
+		{
+			System.out.println("Negative");
+		}
+	}
+}
